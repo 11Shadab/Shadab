d43e1f56f315d77986cff2e85b0649c078d8010d
Shadabakhtar1 <95269781+Shadabakhtar1@users.noreply.github.com>
2022-09-24 19:23:08 +0530

    Create naturalNumberSum.java

---
+++ naturalNumberSum.java
@@ -0,0 +1,11 @@
+public class Main  
+    {  
+        public static void main(String[] args)   
+            {  
+                int i, n=50, sum=0;    
+                    for(i=1;i<=n; ++i)  {
+                            sum=sum+i;  
+                        }  
+            System.out.println("Sum of Natural Numbers is = " + sum);  
+            }  
+    }  
